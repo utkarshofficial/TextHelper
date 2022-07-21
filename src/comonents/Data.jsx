ba917f4fc8affb92ab0ee5e12cd81f5c0f69416a
utkarshofficial <utkarshofficialin@gmail.com>
2022-07-22 01:22:22 +0530

    copy and delete button icon added

--- src/comonents/Data.jsx
+++ src/comonents/Data.jsx
@@ -1,35 +1,40 @@
 import React from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import { Button,ButtonGroup } from "@mui/material";
 
-function Data({ data, removeDataItem, index, showToast}) {
+function Data({ data, removeDataItem, index, showToast }) {
+  const DeleteBtn = (
+    <Button
+      onClick={() => {
+        removeDataItem(index);
+      }}
+    >
+      <DeleteOutlineIcon color="error"/>
+    </Button>
+  );
 
-  
+  const CopyBtn = (
+    <CopyToClipboard text={data} onCopy={showToast}>
+      <Button
+        onClick={() => {
+          showToast();
+        }}
+      >
+        <ContentCopyIcon color="success" />
+      </Button>
+    </CopyToClipboard>
+  );
+
   return (
     <li className="list-group-item textAndBtn">
       <div className="text-item">{data}</div>
 
-      <div className="group-btns">
-        <div
-          className="btn-group"
-          role="group"
-          aria-label="Basic mixed styles example"
-        >
-          <button
-            type="button"
-            className="btn btn-danger"
-            onClick={() => {
-              removeDataItem(index);
-            }}
-          >
-            Delete
-          </button>
-          <CopyToClipboard text={data} onCopy={showToast}>
-            <button type="button" className="btn btn-success" onClick={()=>{showToast()}}>
-              Copy
-            </button>
-          </CopyToClipboard>
-        </div>
-      </div>
+      <ButtonGroup variant="text" aria-label="text button group">
+          {CopyBtn}
+          {DeleteBtn}
+      </ButtonGroup>
     </li>
   );
 }
