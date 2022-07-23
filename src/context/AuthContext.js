b9e9f33a939ea9a19d093c6c30b38bd391131102
utkarshofficial <utkarshofficialin@gmail.com>
2022-07-23 16:00:01 +0530

    Save user on local storage after login

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,14 +1,23 @@
-import { createContext,useReducer } from "react";
+import { createContext,useEffect,useReducer } from "react";
 import AuthReducer from "./AuthReducer"
 
 const INITIAL_STATE = {
-  currentUser: null,
+  // get the user from localStorage if it have
+  // || and if there is no user return null
+  currentUser: JSON.parse(localStorage.getItem("user")) || null,
 };
 
 export const AuthContext = createContext(INITIAL_STATE);
 
 export const AuthContextProvider = ({children}) =>{
   const [state,dispatch] = useReducer(AuthReducer,INITIAL_STATE);
+
+  // to save user to localstorage show user don't want to login again & again
+  useEffect(()=>{
+    localStorage.setItem("user",JSON.stringify(state.currentUser));
+
+    // whenever currentUser change we store it 
+  },[state.currentUser])
 
   return (
     <AuthContext.Provider value={{currentUser: state.currentUser, dispatch}}>
