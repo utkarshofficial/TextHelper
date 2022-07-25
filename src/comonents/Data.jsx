33a0fb9879f791c612dd48231fb82d95ca2fdae5
utkarshofficial <utkarshofficialin@gmail.com>
2022-07-25 18:59:09 +0530

    if data is bigger then shwoing read more / read less feature

--- src/comonents/Data.jsx
+++ src/comonents/Data.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { CopyToClipboard } from "react-copy-to-clipboard";
 import ContentCopyIcon from "@mui/icons-material/ContentCopy";
 import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
-import {
-  Button,
-  ButtonGroup,
-  Link,
-} from "@mui/material";
+import { Button, ButtonGroup, Link } from "@mui/material";
 import "./Data.scss";
 
 function Data({ data, removeDataItem, index, showToast }) {
@@ -24,7 +20,7 @@ function Data({ data, removeDataItem, index, showToast }) {
     <CopyToClipboard text={data}>
       <Button
         onClick={() => {
-          showToast("Item Copied Successfully!","success");
+          showToast("Item Copied Successfully!", "success");
         }}
       >
         <ContentCopyIcon color="success" />
@@ -32,43 +28,38 @@ function Data({ data, removeDataItem, index, showToast }) {
     </CopyToClipboard>
   );
 
-  //array for showing all the data
-  var dataFirst, dataSecond;
-
-  // to set limited char on data
-  // 100 char is seted
-  var newData = data;
-  dataFirst = newData;
-  let maxLimit = 100;
-  if (newData.length > maxLimit) {
-    newData = newData.slice(0, maxLimit) + "... ";
-    dataFirst = newData;
-  }
+  // * for showing limited data on bigger text
+  // * Setting limit of character in data
+  const MAX = 100;
+  const [limitedData, setLimitedData] = React.useState(data.slice(0,MAX));
+  const [currentLimit, setCurrentLimit] = React.useState(MAX);
+  const [showLess,setShowLess] = React.useState(false);
 
   const readMore = () => {
-    newData = data;
-    maxLimit += maxLimit;
-    if (newData.length > maxLimit) {
-      newData = newData.slice(0, maxLimit) + "... ";
-    } else {
-      newData = data;
+    setCurrentLimit(currentLimit + MAX);
+    setLimitedData(data.slice(0, currentLimit));
+    if(!showLess){
+      setShowLess(true);
     }
-    dataFirst = newData;
   };
-  dataSecond =
-    newData.length > maxLimit ? <span onClick={readMore}>more</span> : null;
+  const readLess = ()=>{
+    setLimitedData(data.slice(0,MAX));
+    setCurrentLimit(MAX);
+    setShowLess(false);
+  }
 
-  // link filttering
+  // * Link Filtering
+  let dataFirst = limitedData;
   if (data.includes("http://") || data.includes("https://")) {
     dataFirst = (
       <Link target="_blank"href={data} underline="hover">
-        {data}
+        {limitedData}
       </Link>
     );
   }else if(data.includes("://")){
     dataFirst = (
       <span className="text-primary fw-bold">
-        {data}
+        {limitedData}
       </span>
     );
   }
@@ -77,7 +68,10 @@ function Data({ data, removeDataItem, index, showToast }) {
     <li className="list-group-item textAndBtn">
       <div className="text-item">
         {dataFirst}
-        {dataSecond}
+        {/* Calling readmore if limit breaks */}
+        {(data.length > currentLimit) ? <span onClick={readMore}>...more</span> : null}
+        {/* Calling show less after biger text */}
+        {showLess ? <span onClick={readLess}>...less</span> : null}
       </div>
 
       <ButtonGroup variant="text" aria-label="text button group">
